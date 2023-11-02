bbc9aa2d39610e81f1798af4029330d280dd57c2
igunereve <igunereve@gmail.com>
2023-11-03 00:30:32 +0200

    more updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,12 +4,9 @@ export default function Header() {
   return (
     <header>
       <nav className="nav">
-        <img src={reactLogo} className="nav-logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
+        <img className="nav--icon" src={reactLogo} />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
       </nav>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
 import "./index.css";
-import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
-import MainContent from "./MainContent.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
     <Header />
-    <MainContent />
-    <Footer />
+    <App />
   </React.StrictMode>
 );
